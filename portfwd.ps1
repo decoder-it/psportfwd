569f5ddb71bb350f510436d2bc413499bf11d1e4
decoder-it <35346881+decoder-it@users.noreply.github.com>
2018-06-30 17:52:23 +0200

    First commit

---
+++ portfwd.ps1
@@ -0,0 +1,77 @@
+$mycode = @"
+using System;
+using System.Net;
+using System.Net.Sockets;
+ 
+
+    public class TcpForwarder
+    {
+        private readonly Socket _mainSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+ 
+       public static void StartPortFwd(string localhost, string localport, string remotehost, string remoteport)
+		{
+			new TcpForwarder().Start(
+			     new IPEndPoint(IPAddress.Parse(localhost), int.Parse(localport)),
+                 new IPEndPoint(IPAddress.Parse(remotehost), int.Parse(remoteport))
+                 );
+        }
+        public void Start(IPEndPoint local, IPEndPoint remote)
+        {
+            _mainSocket.Bind(local);
+            _mainSocket.Listen(10);
+ 
+            while (true)
+            {
+                var source = _mainSocket.Accept();
+                var destination = new TcpForwarder();
+                var state = new State(source, destination._mainSocket);
+                destination.Connect(remote, source);
+                source.BeginReceive(state.Buffer, 0, state.Buffer.Length, 0, OnDataReceive, state);
+            }
+        }
+ 
+        private void Connect(EndPoint remoteEndpoint, Socket destination)
+        {
+            var state = new State(_mainSocket, destination);
+            _mainSocket.Connect(remoteEndpoint);
+            _mainSocket.BeginReceive(state.Buffer, 0, state.Buffer.Length, SocketFlags.None, OnDataReceive, state);
+        }
+ 
+        private static void OnDataReceive(IAsyncResult result)
+        {
+            var state = (State)result.AsyncState;
+            try
+            {
+                var bytesRead = state.SourceSocket.EndReceive(result);
+                if (bytesRead > 0)
+                {
+                    state.DestinationSocket.Send(state.Buffer, bytesRead, SocketFlags.None);
+                    state.SourceSocket.BeginReceive(state.Buffer, 0, state.Buffer.Length, 0, OnDataReceive, state);
+                }
+            }
+            catch
+            {
+                state.DestinationSocket.Close();
+                state.SourceSocket.Close();
+            }
+        }
+ 
+        private class State
+        {
+            public Socket SourceSocket { get; private set; }
+            public Socket DestinationSocket { get; private set; }
+            public byte[] Buffer { get; private set; }
+ 
+            public State(Socket source, Socket destination)
+            {
+                SourceSocket = source;
+                DestinationSocket = destination;
+                Buffer = new byte[8192];
+            }
+        }
+    }
+
+"@
+Add-Type -TypeDefinition $mycode
+#.\portwd.ps1 127.0.0.1 8080 192.168.1.100 80
+[TcpForwarder]::StartPortFwd($args[0],$args[1],$args[2],$args[3])
